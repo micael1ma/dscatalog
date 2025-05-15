ff200584121cb3a714857c06ebcfa4ae3002dc6e
micael1ma <micael1ma.professional@gmail.com>
2025-05-15 15:40:46 -0300

    Database seeding

--- backend/src/main/java/com/micael1ma/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/micael1ma/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import com.micael1ma.dscatalog.entities.Category;
 import com.micael1ma.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,11 +14,10 @@ public class CategoryService {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Transactional(readOnly = true)
     public List<Category> findAll(){
         return categoryRepository.findAll();
     }
 
-    public void save(Category category){
-        categoryRepository.save(category);
-    }
+
 }
