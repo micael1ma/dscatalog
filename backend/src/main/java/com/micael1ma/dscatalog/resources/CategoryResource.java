99dbe17b461bb6931c30312fd2b31f7df0eadf66
micael1ma <micael1ma.professional@gmail.com>
2025-05-18 15:01:01 -0300

    Product entity and database seed

--- backend/src/main/java/com/micael1ma/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/micael1ma/dscatalog/resources/CategoryResource.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categories")
@@ -26,10 +25,8 @@ public class CategoryResource {
             @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
             @RequestParam(value = "direction", defaultValue = "ASC") String direction,
             @RequestParam(value = "orderBy", defaultValue = "name") String orderBy
-
     ) {
         PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
-
         Page<CategoryDTO> list = service.findAllPaged(pageRequest);
         return ResponseEntity.ok().body(list);
     }
